f94227abb82edbd41038c8fabe7ce3d614dcaa28
Paco Gascón <pgascon80@gmail.com>
2023-11-26 18:54:17 +0100

    DBZ - Video 63

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
 
   constructor() { }
 
